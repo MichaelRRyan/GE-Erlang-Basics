d49faeb17711ecead01c2d5e979cce6e7073b220
Michael R Ryan <c00239510@itcarlow.ie>
2022-01-13 11:07:01 +0000

    Added achievement deletion

--- records.erl
+++ records.erl
@@ -12,16 +12,25 @@ run()->
     NewGiggles = update_record(DrGiggles, 10, ["Joined Game"]),
     NewDuke = update_record(DukeNuke, 10, ["Joined Game"]),
 
-    io:fwrite("Updated the records, new records:\n"),
+    io:fwrite("\nUpdated the records, new records:\n\n"),
 
     output_record(NewGiggles),
-    output_record(NewDuke).
+    output_record(NewDuke),
+
+    Deleted = remove_achievements(NewGiggles, ["Quest 1"]),
+
+    io:fwrite("\nDeleting an achievement, new record:\n\n"),
+
+    output_record(Deleted).
 
 create_record(GamerTag, Points, Achievements)->
     #gamer{ gamer_tag = GamerTag, points = Points, achievements = Achievements }.
 
 update_record(Gamer, NewPoints, NewAchievements)->
     Gamer#gamer{ points = Gamer#gamer.points + NewPoints, achievements = lists:append(Gamer#gamer.achievements, NewAchievements) }.
+
+remove_achievements(Gamer, Achievements)->
+    Gamer#gamer{ points = Gamer#gamer.points, achievements = Gamer#gamer.achievements -- Achievements }.
 
 output_record(Gamer)->
     io:fwrite("~p", [Gamer#gamer.gamer_tag]),
